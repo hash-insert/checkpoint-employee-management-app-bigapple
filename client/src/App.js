a46f089c4540601572ed0f9dc66d97430d56361e
Nikhil Kavuri <nikhilkavuri1503@gmail.com>
2023-07-19 21:47:26 +0530

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar';
 import {Routes,Route} from "react-router-dom"
 import ProfilePage from './pages/profile';
 import './App.css';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <nav><Navbar /></nav>
       <Routes>
+        <Route path='/' element={<HomePage />}></Route>
         <Route path="/profile" element={<ProfilePage />} ></Route>
       </Routes>
     </div>
